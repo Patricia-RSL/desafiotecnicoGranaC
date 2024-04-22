65f9438e5f5e9528f8017650a9d5e4950976660f
Patricia Raposo Santana Lima <36173425+Patricia-RSL@users.noreply.github.com>
2024-04-22 14:55:03 -0300

    Create Desafio.js

---
+++ Desafio.js
@@ -0,0 +1,61 @@
+function calculate(expr) {
+    expr = expr.split('-').join('+-');
+    exprList = expr.split("+")
+    //console.log("exprList " + exprList)
+    soma=0
+    for (var i = 0; i < exprList.length; i++) {
+        result = Number(calculateMult(exprList[i]))
+        //console.log("result " + result)       
+        soma += result
+        //console.log("soma: "+soma)
+    }
+    
+    return soma
+}
+
+function calculateMult(expr) {
+    exprMult = expr.split('*')
+    //console.log("exprMult " + exprMult)
+    if (exprMult.length==1 && !expr.includes('/')) { 
+        return expr
+    }
+    mult=1
+    //console.log(exprMult)
+    for (var i = 0; i < exprMult.length; i++) {
+        result = Number(calculateDiv(exprMult[i]))
+        //console.log("result " + result)       
+        mult *= result
+    }
+    
+    return mult
+}
+
+function calculateDiv(expr) {
+    exprDiv = expr.split("/")  
+    //console.log("exprDiv " + exprDiv)  
+    if (exprDiv.length==1) {
+        return expr
+    }
+    div=exprDiv[0]
+    for (var i = 1; i < exprDiv.length; i++) {
+        div /= Number(exprDiv[i])
+    }
+    
+    return div
+}
+
+
+function test(expr, expected) {
+    const actual = calculate(expr)
+    const result = actual === expected ? "passed" : `failed. Actual: ${actual}`
+    console.log(`Test case ${expr}: ${result}`)
+}
+
+test('1+2', 8)
+test('0+8', 8)
+test('10+350', 8)
+test('5+40+100', 8)
+test('10-2', 8)
+test('-2+10', 8)
+test('2*4', 8)
+test('12/3', 8)
